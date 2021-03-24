8035ccb6ffc3c46c0af50af1763a86c5c8707513
Darkshredder <srivastavayash58@gmail.com>
2021-03-24 13:11:53 +0530

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(process.env.GOOGLE_ANALYTICS_TRACKING_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
